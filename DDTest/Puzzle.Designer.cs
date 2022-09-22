0d76fb915137af2bbfa8e06952fc47b7d4f1ad5e
animorphix <ianchernyi@gmail.com>
2022-09-22 13:48:47 +0300

    First working solution

---
+++ DDTest/Puzzle.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace DDTest
+{
+    partial class Puzzle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EnterButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox1.Location = new System.Drawing.Point(287, 139);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(252, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(292, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 50);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter puzzle size";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // EnterButton
+            // 
+            this.EnterButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.EnterButton.Location = new System.Drawing.Point(287, 209);
+            this.EnterButton.Name = "EnterButton";
+            this.EnterButton.Size = new System.Drawing.Size(252, 36);
+            this.EnterButton.TabIndex = 2;
+            this.EnterButton.Text = "Enter";
+            this.EnterButton.UseVisualStyleBackColor = true;
+            this.EnterButton.Click += new System.EventHandler(this.EnterButton_Click);
+            // 
+            // Puzzle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 1250);
+            this.Controls.Add(this.EnterButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Puzzle";
+            this.Text = "Puzzle";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Button EnterButton;
+    }
+}
---
+++ DDTest/Puzzle.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DDTest
+{
+    public partial class Puzzle : Form
+    {
+        public Puzzle()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        int n;
+        private PictureBox[,] Safe = null;
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int size = Int32.Parse(textBox1.Text);
+                if (size > 1 && size <= 8)
+                    n = size;
+                else
+                {
+                    MessageBox.Show("Enter values between 1 and 8");
+                    n = 0;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Enter values between 1 and 8");
+            }
+
+
+            if (Safe != null)
+                foreach (var pictureBox in Safe)
+                    Controls.Remove(pictureBox);
+
+            Safe = new PictureBox[n, n];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    Safe[i, j] = new PictureBox();
+                    Safe[i, j].Location = new System.Drawing.Point(-58*n +474 + i * 100, 300 + j * 100);
+                    Safe[i, j].Size = new System.Drawing.Size(80, 80);
+                    Safe[i, j].TabIndex = i;
+                    Safe[i, j].BackColor = Color.Black;
+                    Safe[i, j].SizeMode = PictureBoxSizeMode.StretchImage;
+                    Safe[i, j].Image = Properties.Resources.Handle;
+                    Safe[i, j].Tag = 0;
+                    Safe[i, j].Anchor = AnchorStyles.Top;
+                    Controls.Add(Safe[i, j]);
+                    int row = i;
+                    int column = j;
+                    Safe[i, j].Click += (x, y) => { SafeClick(row, column); };
+                }
+
+            var rnd = new Random();
+            for (int i = 0; i < 40; i++)
+                SafeClick(rnd.Next(n), rnd.Next(n), true);
+        }
+
+        void SafeClick(int row, int column, bool init = false)
+        {
+            bool win = !init;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == row || j == column)
+                    {
+                        Image image = Safe[i, j].Image;
+                        image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                        Safe[i, j].Image = image;
+
+                        Safe[i, j].Tag = ((int)Safe[i, j].Tag + 1) % 2;
+                    }
+
+                    win &= (int)Safe[i, j].Tag == (int)Safe[0, 0].Tag;
+                }
+
+            if (win) MessageBox.Show("win");
+
+        }
+    }
+}
